d124fc398999ffa9bcce343b406988f33b00e5a1
Bob Carroll <Bob Carroll>
2017-08-10 18:08:41 -0400

    checking connection output

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,22 @@
 
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
   user: "root",
   password: "Stacked808",
   database: "burgers_db"
-});
+  });  
+}
+console.log(connection);
+
+
 
 // Make connection.
 connection.connect(function(err) {
